4fbe27814e78825309edfdd82341299a5c7c330d
Conor Barr <conorlbarr@gmail.com>
2021-01-10 15:35:05 -0500

    Initial commit of DotNetExtensions project

---
+++ DotNetExtensions/ExceptionExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetExtensions
+{
+    public static class ExceptionExtension
+    {
+        public static List<Exception> GetExceptions(this Exception e)
+        {
+            List<Exception> exceptions = new List<Exception>();
+            while (e != null)
+            {
+                exceptions.Add(e);
+                e = e.InnerException;
+            }
+            return exceptions;
+        }
+    }
+}
