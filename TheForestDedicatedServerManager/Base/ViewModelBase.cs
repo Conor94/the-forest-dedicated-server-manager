e2541d2b370ce0382535c21aad756e70c3d61e90
Conor Barr <conorlbarr@gmail.com>
2021-01-17 16:42:13 -0500

    IContainerProvider dependency added to ViewModelBase class

--- TheForestDedicatedServerManager/Base/ViewModelBase.cs
+++ TheForestDedicatedServerManager/Base/ViewModelBase.cs
@@ -1,6 +1,8 @@
 ﻿using Prism.Events;
+using Prism.Ioc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using TheForestDedicatedServerManager.Events;
 
 namespace TheForestDedicatedServerManager.Base
@@ -11,33 +13,37 @@ namespace TheForestDedicatedServerManager.Base
     public abstract class ViewModelBase : DataErrorBindableBase
     {
         #region Fields and properties
-        private string mTitle;
+        private IContainerProvider mContainer;
         private IEventAggregator mEventAggregator;
-        public string Title
+        private string mTitle;
+
+        public IContainerProvider Container
         {
-            get => mTitle;
-            set
-            {
-                SetProperty(ref mTitle, value);
-            }
+            get => mContainer;
+            private set => mContainer = value;
         }
         public IEventAggregator EventAggregator
         {
             get => mEventAggregator;
             private set => mEventAggregator = value;
         }
-
+        public string Title
+        {
+            get => mTitle;
+            set => SetProperty(ref mTitle, value);
+        }
         #endregion
 
         #region Constructors
         /// <inheritdoc cref="DataErrorBindableBase(Dictionary&lt;string, Func&lt;object, string&lt;&lt; _validators)"/>
-        public ViewModelBase(IEventAggregator eventAggregator) : this(eventAggregator, null)
+        public ViewModelBase(IEventAggregator eventAggregator, IContainerProvider container) : this(eventAggregator, container, null)
         {
         }
 
         /// <inheritdoc cref="DataErrorBindableBase(Dictionary&lt;string, Func&lt;object, string&lt;&lt; _validators)"/>
-        public ViewModelBase(IEventAggregator eventAggregator, Dictionary<string, Func<object, string>> validators) : base(validators)
+        public ViewModelBase(IEventAggregator eventAggregator, IContainerProvider container, Dictionary<string, Func<object, string>> validators) : base(validators)
         {
+            Container = container;
             EventAggregator = eventAggregator;
             Title = "";
         }
